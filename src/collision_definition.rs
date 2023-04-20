a0b2b2bb3fcfb104bb852cffe937da0835c0f74f
castroploiin <fieryfire1005@gmail.com>
2023-04-21 00:35:07 +0400

    cargo fix 5

--- src/collision_definition.rs
+++ src/collision_definition.rs
@@ -3,18 +3,18 @@ use crate::rigidbodies::{RigidBody, Updateable, HandleData};
 use crate::world::*;
 
 #[derive(Debug)]
-pub enum CollisionType<T: Updateable + HandleData<T>> {
+pub enum CollisionType<T> where T: Updateable + HandleData<T> + AsRef<T> {
     ObjObj(T, T),
     ObjWorld(T, World<T>),
 }
 
 #[derive(Debug)]
-pub struct Collision<T: Updateable + HandleData<T>> {
+pub struct Collision<T> where T: Updateable + HandleData<T> + AsRef<T> {
     objects: CollisionType<T>,
     time: f64,
 }
 
-impl<T: Updateable + HandleData<T>> CollisionType<T> {
+impl<T> CollisionType<T> where T: Updateable + HandleData<T> + AsRef<T>{
     fn get_participants(&self) -> (T, Option<T>) {
         match self {
             CollisionType::ObjObj(body1, body2) => (*body1, Some(*body2)),
@@ -23,7 +23,7 @@ impl<T: Updateable + HandleData<T>> CollisionType<T> {
     }
 }
 
-impl<T: Updateable + HandleData<T>> Collision<T> {
+impl<T> Collision<T> where T: Updateable + HandleData<T> + AsRef<T> {
     pub fn get_time(&self) -> f64 {
         self.time
     }
