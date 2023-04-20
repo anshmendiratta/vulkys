d9d9b9ff8380ccaad49883634adef1dc942405c3
castroploiin <fieryfire1005@gmail.com>
2023-04-21 00:28:29 +0400

    Refactor: Before cargo fix 3

--- src/step.rs
+++ src/step.rs
@@ -1,18 +1,18 @@
-// use crate::motion::{Physics, PhysicsMath};
-// use crate::world::*;
-// use crate::rigidbodies::*;
+use crate::motion::{Physics, PhysicsMath};
+use crate::world::*;
+use crate::rigidbodies::*;
 
-// pub fn step<T: Updateable + HandleData<T>>(world: World<T>, dt: f64) {
-//     let dt: f64 = world.get_timestep();
+pub fn step<T: Updateable + HandleData<T> + AsRef<T>>(world: World<T>, dt: f64) {
+    let dt: f64 = world.get_timestep();
 
-//     for object in world.get_objects() {
-//         match object {
-//             RigidBody => (),
-//             Ball => {
-//                 Physics::update_angular_velocity(object);
-//                 Physics::update_velocity(object, dt);
-//                 Physics::update_position(object, dt);
-//             }
-//         }
-//     }
-// }
+    for object in world.get_objects() {
+        match object {
+            RigidBody => (),
+            Ball => {
+                Physics::update_angular_velocity(object);
+                Physics::update_velocity(object, dt);
+                Physics::update_position(object, dt);
+            }
+        }
+    }
+}
